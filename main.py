94f84970f7386ebe5c6535b0a0a8ea5a434e2314
ghot <eirefigure@gmail.com>
2018-12-11 19:24:45 -0500

    final

--- main.py
+++ main.py
@@ -1,30 +1,21 @@
-# import pyechonest
 import spotipy
 import spotipy.util as utility
-import sys
-import os
-import webbrowser
-import json
-from json.decoder import JSONDecodeError
 import pandas as pd
-import matplotlib.pyplot as plt
-import pickle
 
 from sklearn.metrics import accuracy_score
 from sklearn.model_selection import train_test_split
 import pandas as panda
-from sklearn.neighbors import KNeighborsClassifier
 
-# username = sys.argv[1]
 from sklearn.tree import DecisionTreeClassifier
+import matplotlib.pyplot as plt
 
 clientId = "ce7456ff192c41a28dd9b8bc55bb845c"
 secret = "55bbc589f0d64cd0b711b309320c5f98"
 scope = "user-library-read playlist-read-private playlist-modify-private playlist-read-collaborative playlist-modify-public"
 redirectURI = 'http://google.com/'
 username = 'ahmadaram34'
-badPlaylistId = '5W6pA4C8gC7T5Kdx4wBjU6'
-goodPlaylistId = '3LiHlnnWae4GaeJ5mfZacG'
+badPlaylistId = '4SVPwFqykhFf5fqiIIBc21'
+goodPlaylistId = '4cAyKcVe0iUmFoorM3XpEL'
 
 # this gets the authentiaction token for us to access the account through spotify
 try:
@@ -41,16 +32,15 @@ user = Spotifyobj.current_user()
 print(user)
 
 
-# print(json.dump(user, sort_keys=True, indent=4))
-
-
 def add_to_bad_playlist():
-    badMusic = ["37i9dQZF1DX1lVhptIYRda"]
+    badMusic = ["37i9dQZF1DWTcqUzwhNmKv", "37i9dQZF1DX1lVhptIYRda",
+                "37i9dQZF1DX0MuOvUqmxDz", "37i9dQZF1DX8S0uQvJ4gaa",
+                "37i9dQZF1DX0MuOvUqmxDz", "37i9dQZF1DWYiR2Uqcon0X"]
     for playlist in badMusic:
         sourcePlaylist = Spotifyobj.user_playlist(username, playlist)
         tracks = sourcePlaylist["tracks"]
         songs = tracks["items"]
-        while tracks['next']:
+        while tracks["next"]:
             tracks = Spotifyobj.next(tracks)
             for item in tracks["items"]:
                 songs.append(item)
@@ -63,7 +53,8 @@ def add_to_bad_playlist():
 
 
 def add_to_good_playlist():
-    goodMusic = {"37i9dQZEVXbLRQDuF5jeBp"}
+    goodMusic = {"37i9dQZF1DX4dyzvuaRJ0n", "37i9dQZF1DX0BcQWzuB7ZO", "37i9dQZF1DX8tZsk68tuDw",
+                 "37i9dQZF1DX0hvSv9Rf41p", "37i9dQZF1DXaXB8fQg7xif", "37i9dQZF1DXcZDD7cfEKhW"}
 
     for playlist in goodMusic:
         sourcePlaylist = Spotifyobj.user_playlist(username, playlist)
@@ -73,10 +64,8 @@ def add_to_good_playlist():
             tracks = Spotifyobj.next(tracks)
             for item in tracks["items"]:
                 songs.append(item)
-        ids = []
         print(len(songs))
         print(songs[0]['track']['id'])
-        i = 0
         for i in range(len(songs)):
             Spotifyobj.user_playlist_add_tracks(username, goodPlaylistId, [songs[i]["track"]["id"]])
 
@@ -91,9 +80,8 @@ def getGoodSongsIdFeatures():
         for item in good_trackList["items"]:
             good_songList.append(item)
     goodSongIDs = []
-    for i in range(len(good_songList)):
+    for i in range(len(good_songList) - 500):
         goodSongIDs.append(good_songList[i]['track']['id'])
-    #print(goodSongIDs)
 
     features = []
     for i in range(0, len(goodSongIDs)):
@@ -104,7 +92,7 @@ def getGoodSongsIdFeatures():
     print(features)
     return features
 
-    #print(features)
+
 def bad_playlist_idsFeatures():
     tracks = Spotifyobj.user_playlist(username, badPlaylistId)['tracks']
     songs = tracks['items']
@@ -113,9 +101,8 @@ def bad_playlist_idsFeatures():
         tracks = Spotifyobj.next(tracks)
         songs += [x for x in tracks['items']]
     ids = []
-    for i in range(len(songs)):
+    for i in range(len(songs) - 500):
         ids.append(songs[i]['track']['id'])
-    #print(ids)
 
     features = []
     for i in range(0, len(ids)):
@@ -124,21 +111,28 @@ def bad_playlist_idsFeatures():
             features.append(track)
             features[-1]['target'] = 0
     print(features)
-    #print(features)
     return features
 
+
+def graphs(data, type, title, x=None, y=None, *args):
+    fig = plt.figure(figsize=(12, 8))
+    plt.title(title)
+    plt.xlabel(x)
+    plt.ylabel(y)
+    data[data['target'] == 1][type].hist(alpha=args[0], bins=args[1], label=args[2])
+    data[data['target'] == 0][type].hist(alpha=args[0], bins=args[1], label=args[3])
+    plt.legend(loc='upper right')
+    plt.show()
 
 
 def TrainTestClassification(goodSongFeatures,badSongFeatures):
     bad = panda.DataFrame(badSongFeatures)
     good = panda.DataFrame(goodSongFeatures)
-    bad = bad.drop_duplicates()
-    good = good.drop_duplicates()
     merge = [bad,good]
     trainingData = panda.concat(merge)
     #trainingData = panda.merge(bad,good)
 
-    train,test = train_test_split(trainingData,test_size=0.15)
+    train,test = train_test_split(trainingData,test_size=0.40)
     features = ["danceability","loudness","valence","acousticness","key","instrumentalness",
                 "acousticness","speechiness"]
     train1 = train[features]
@@ -147,12 +141,16 @@ def TrainTestClassification(goodSongFeatures,badSongFeatures):
     test2 = test["target"]
 
     tree = DecisionTreeClassifier(min_samples_split=100)
-    tree.fit(train1,train2)
+    tree.fit(train1, train2)
     predict1 = tree.predict(test1)
     accy = accuracy_score(test2,predict1) *100
     print("Accuracy for Decision Tree:",round(accy,1),"%")
 
-
+    graphs(trainingData, 'tempo', 'Good and Bad Song Tempo', 'Number of Features', 'Tempo', 0.7, 30, 'good songs', 'bad songs')
+    graphs(trainingData, 'valence', 'Valence of Songs', 'Number of Features', 'Valence', 0.5, 30, 'good songs', 'bad songs')
+    graphs(trainingData, 'loudness', 'Loudness of Songs', 'Number of Features', 'Loudness', 0.5, 30, 'good songs', 'bad songs')
+    graphs(trainingData, 'trackPopularity', 'Popularity of Songs', 'Number of Features', 'Popularity', 0.5, 30, 'good songs', 'bad songs')
+    graphs(trainingData, 'key', 'Key Signature of Songs', 'Number of Features', 'Key', 0.5, 30, 'good songs', 'bad songs')
 
 
 print("ADDING SONGS TO BAD SONGS")
@@ -161,26 +159,13 @@ print("ADDING TO GOOD SONGS")
 #add_to_good_playlist()
 print("GETTING GOOD SONGS")
 goodSongFeatures = getGoodSongsIdFeatures()
-print("GETTING BAD SONGS FEATURES")
 badSongFeatures = bad_playlist_idsFeatures()
 TrainTestClassification(goodSongFeatures,badSongFeatures)
-
 
 goodFrame = pd.DataFrame(goodSongFeatures)
 badFrame = pd.DataFrame(badSongFeatures)
 
 # print(goodFrame)
-_,ax = plt.subplots()
-ax.set_xlabel('valence')
-ax.set_ylabel('features')
-ax.set_title(r'Good Song Valence')
-plt.hist(goodFrame['valence'])
-_, bx = plt.subplots()
-bx.set_xlabel('valence')
-bx.set_ylabel('features')
-bx.set_title(r'Bad Song Valence')
-plt.hist(badFrame['valence'])
-plt.show()
 # goodFrame.plot(x=, y=goodFrame['valence'])
 # badFrame.plot()
 features = pd.merge(goodFrame, badFrame)
